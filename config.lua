c783980cb131c6ebf850a80032c8724fbc708170
Trecto34 <trecto34@protonmail.com>
2024-02-24 09:32:35 -0300

    Fix typo on templates

--- config.lua
+++ config.lua
@@ -62,12 +62,13 @@ lvim.plugins = {
       end
       return tostring(os.time()) .. "-" .. suffix
     end,
-  },
-  templates = {
-    subdir = "Templates",
-    date_format = "%Y-%m-%d",
-    time_format = "%H:%M",
-    -- A map for custom variables, the key should be the variable and the value a function
-    substitutions = {},
+
+    templates = {
+      subdir = "Templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+    },
   },
 }
+
